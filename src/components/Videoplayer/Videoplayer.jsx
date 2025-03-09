8b30695fb91dd8146984945178da7de99681c59d
Armin <151685402+13Armin85@users.noreply.github.com>
2025-03-09 10:11:56 +0330

    changes

--- src/components/Videoplayer/Videoplayer.jsx
+++ src/components/Videoplayer/Videoplayer.jsx
@@ -2,6 +2,13 @@
 
 import { useRef, useState } from "react";
 import style from "./Videoplayer.module.css";
+import icon1 from "../../../public/icons/Vector.png";
+import icon2 from "../../../public/icons/Group 48095490.png";
+import icon3 from "../../../public/icons/next.png";
+import icon4 from "../../../public/icons/volume-high.png";
+import icon5 from "../../../public/icons/setting-2.png";
+import icon6 from "../../../public/icons/maximize-2.png";
+import Image from "next/image";
 
 const CustomVideoPlayer = () => {
   const videoRef = useRef(null);
@@ -19,21 +26,34 @@ const CustomVideoPlayer = () => {
   };
 
   return (
-    <div className={style.container}>
-      <video ref={videoRef} className={style.element}>
-        <source src="/sample.mp4" type="video/mp4" />
-      </video>
-      <div className={style.contorol}>
-        <button onClick={togglePlay} className={style.button}>
-          {isPlaying ? "⏸" : "▶"}
-        </button>
-        <span className={style.time}>0:51</span>
-        <div className={style.progress}>
-          <div className={style.bar} style={{ width: `${progress}%` }}></div>
-        </div>
-        <span className={style.time}>2:31</span>
+    <section className={style.container}>
+      <div className={style.video}>
+        <video ref={videoRef} className={style.element}>
+          <source src="/sample.mp4" type="video/mp4" />
+        </video>
       </div>
-    </div>
+
+      <div className={style.contorol}>
+        <div className={style.pause}>
+          <Image src={icon1} />
+        </div>
+        <div className={style.bar}>
+          <Image src={icon2} />
+        </div>
+        <div className={style.next}>
+          <Image src={icon3} />
+        </div>
+        <div className={style.volume}>
+          <Image src={icon4} />
+        </div>
+        <div className={style.setting}>
+          <Image src={icon5} />
+        </div>
+        <div className={style.maximize}>
+          <Image src={icon6} />
+        </div>
+      </div>
+    </section>
   );
 };
 
