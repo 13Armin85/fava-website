cefb2fb29262cc5d9e6ebf7acb2dd9ed7a00033f
Armin <151685402+13Armin85@users.noreply.github.com>
2025-03-16 16:19:54 +0330

    update

--- src/components/customer/Customer.jsx
+++ src/components/customer/Customer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
 import style from "./Customer.module.css";
 import icon1 from "../../../public/icons/1 4.svg";
@@ -7,63 +8,77 @@ import icon3 from "../../../public/icons/33 1.svg";
 import icon4 from "../../../public/icons/555 1.svg";
 import icon5 from "../../../public/icons/666 1.svg";
 import icon6 from "../../../public/icons/Ellipse 29.png";
-import left from "../../../public/icons/arrow-left.png";
-import right from "../../../public/icons/arrow-right.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 const Customer = () => {
+  const swiperRef = useRef(null);
+
   return (
-    <section className={style.clientsec}>
-      <h2>مشتریان</h2>
-
-      <div className={style.slider}>
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon1} alt="جتکو" />
-          </div>
-          <p>جتکو</p>
-        </div>
-
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon2} alt="ساپکو" />
-          </div>
-          <p>ساپکو</p>
-        </div>
-
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon3} alt="ایکاپ" />
-          </div>
-          <p>ایکاپ</p>
-        </div>
-
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon4} alt="ساوه و ساختمان" />
-          </div>
-          <p>ساوه و ساختمان</p>
-        </div>
-
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon5} alt="ایران خودرو" />
-          </div>
-          <p>ایران خودرو</p>
-        </div>
-
-        <div className={style.item}>
-          <div className={style.logo}>
-            <Image width={120} height={120} src={icon6} alt="تام" />
-          </div>
-          <p>تام</p>
-        </div>
+    <>
+      <div className={style.header}>مشتــــریان</div>
+      <div className={style.sliderContainer}>
+        <button
+          className={style.prev}
+          onClick={() => swiperRef.current?.slidePrev()}
+        >
+          ‹
+        </button>
+        <Swiper
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          modules={[Navigation]}
+          className={style.myswiper}
+          slidesPerView={4}
+          spaceBetween={20}
+          centeredSlides={true}
+        >
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon1} width={100} height={100} alt="جتکو" />
+            <div className={style.text}>
+              <p>جتکو</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon2} width={100} height={100} alt="ساپکو" />
+            <div className={style.text}>
+              <p>ساپکو</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon3} width={100} height={100} alt="ایکاپ" />
+            <div className={style.text}>
+              <p>ایکاپ</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon4} width={100} height={100} alt="ساوه و ساختمان" />
+            <div className={style.text}>
+              <p>ساوه و ساختمان</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon5} width={100} height={100} alt="ایران خودرو" />
+            <div className={style.text}>
+              <p>ایران خودرو</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiperslide}>
+            <Image src={icon6} width={100} height={100} alt="تام" />
+            <div className={style.text}>
+              <p>تام</p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <button
+          className={style.next}
+          onClick={() => swiperRef.current?.slideNext()}
+        >
+          ›
+        </button>
       </div>
-
-      <div className={style.arrows}>
-        <Image width={120} height={120} alt="pic" src={left} />
-        <Image width={120} height={120} alt="pic" src={right} />
-      </div>
-    </section>
+    </>
   );
 };
 
